b980d43d100fa48bfd07bbf807df8e3e650d5ba1
yoruneko <32332838+yiqingfann@users.noreply.github.com>
2023-06-04 15:51:59 -0700

    feat: add rss (#2)
    * feat: add rss
    
    * debug: update rss link
    
    * fix: change vercel adapter to static for rss
    
    * fix: change rss site url to prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel({
+  output: "static",
+  adapter: vercelStatic({
     analytics: true,
   }),
 });
--- src/pages/posts/chat3gpt.md
+++ src/pages/posts/chat3gpt.md
@@ -2,7 +2,7 @@
 layout: "../../layouts/PostLayout.astro"
 
 title: "用 T3 Stack 做个 ChatGPT Clone"
-createdAt: "2023/05/21"
+pubDate: "2023/05/21"
 ---
 
 ## 为什么做这个 project？
